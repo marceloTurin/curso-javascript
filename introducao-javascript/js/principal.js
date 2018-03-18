f43a1ff4bef0bee36b19188ebd70d2913c85b0bb
Marcelo da Silva Sousa <marcelo.sousa1997@hotmail.com>
2018-03-18 19:41:08 -0300

    Aula 2: Operadores Logicos

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -23,31 +23,6 @@ var altura = tdAltura.textContent;
 //Seleciona a td imc do paciente
 var tdImc = paciente.querySelector(".info-imc");
 
-//Variaveis de verificação
-var pesoEhValido = true;
-var alturaEhValida = true;
 
-//Verifica Peso
-if (peso <= 0 || peso >= 1000) {
-	console.log("Peso invalido");
-	pesoEhValido = false;
-	tdImc.textContent = "Peso Invalido"
-}
-
-//Verifica Altura
-if (altura <= 0 || altura >= 3.00) {
-	console.log("Altura invalida");
-	alturaEhValida = false;
-	tdImc.textContent = "Altura invalida";
-}
-
-//Verifica se as variaveis de verificação são true para fazer o calculo de IMC
-if (alturaEhValida && pesoEhValido) {
-	//Calcula o IMC
-	var imc = peso / (altura * altura);
-
-	// Coloca o conteudo de texto da var imc na tdImc
-	tdImc.textContent = imc;
-}
 
 
