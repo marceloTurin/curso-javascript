7b3d73cfa6da4b9e63fc5b34f7a9e4fbb0cf8ca8
Marcelo da Silva Sousa <marcelo.sousa1997@hotmail.com>
2018-03-18 17:01:18 -0300

    Aula 2: Buscando dados do paciente

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,9 +1,27 @@
 //querySelector seleciona um elemento html,class,id
 var titulo = document.querySelector(".titulo");
-console.log(titulo);
 
-//textContent seleciona o conteúdo de texto da tag/variavel
-console.log(titulo.textContent);
+
+
 titulo.textContent = "Aparecida Nutricionista";
 
-console.log("Fui carregado de um arquivo externo");
+//Seleciona a tr do paciente
+var paciente  = document.querySelector("#primeiro-paciente");
+
+//Seleciona a td peso do paciente
+var tdPeso = paciente.querySelector(".info-peso");
+
+//Seleciona o conteudo de texto da td peso
+var peso = tdPeso.textContent;
+
+//Seleciona a td altura do paciente
+var tdAltura = paciente.querySelector(".info-altura");
+
+//Seleciona o conteudo de texto da td altura
+var altura = tdAltura.textContent;
+
+//Calcula o IMC
+var imc = peso / (altura * altura);
+
+console.log(imc);
+
