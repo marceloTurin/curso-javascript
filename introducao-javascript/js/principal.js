6c256f073b6e2741cea69c7b5bda8da7015b57d8
Marcelo da Silva Sousa <marcelo.sousa1997@hotmail.com>
2018-03-18 20:31:33 -0300

    Aula 03: Array: Replicando a validação e o cálculo do IMC para todos os pacientes

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -5,24 +5,57 @@ var titulo = document.querySelector(".titulo");
 
 titulo.textContent = "Aparecida Nutricionista";
 
-//Seleciona a tr do paciente
-var paciente  = document.querySelector("#primeiro-paciente");
+//Seleciona todas tr dos pacientes
+var pacientes  = document.querySelectorAll(".paciente");
 
-//Seleciona a td peso do paciente
-var tdPeso = paciente.querySelector(".info-peso");
+//Retorna o tamanho do array
+console.log(pacientes.length);
 
-//Seleciona o conteudo de texto da td peso
-var peso = tdPeso.textContent;
+//Criação do for para percorrer um paciente de cada vez
+for ( var i = 0; i < pacientes.length ; i++){
+	
+	var paciente = pacientes[i];
 
-//Seleciona a td altura do paciente
-var tdAltura = paciente.querySelector(".info-altura");
+	//Seleciona a td peso do paciente
+	var tdPeso = paciente.querySelector(".info-peso");
+	//Seleciona o conteudo de texto da td peso
+	var peso = tdPeso.textContent;
 
-//Seleciona o conteudo de texto da td altura
-var altura = tdAltura.textContent;
+	//Seleciona a td altura do paciente
+	var tdAltura = paciente.querySelector(".info-altura");
+	//Seleciona o conteudo de texto da td altura
+	var altura = tdAltura.textContent;
 
-//Seleciona a td imc do paciente
-var tdImc = paciente.querySelector(".info-imc");
+	//Seleciona a td imc do paciente
+	var tdImc = paciente.querySelector(".info-imc");
 
+	//Variaveis de verificação
+	var pesoEhValido = true;
+	var alturaEhValida = true;
 
+	//Verifica Peso
+	if (peso <= 0 || peso >= 1000) {
+		console.log("Peso invalido");
+		pesoEhValido = false;
+		tdImc.textContent = "Peso Invalido"
+	}
 
+	//Verifica Altura
+	if (altura <= 0 || altura >= 3.00) {
+		console.log("Altura invalida");
+		alturaEhValida = false;
+		tdImc.textContent = "Altura invalida";
+	}
+
+	//Verifica se as variaveis de verificação são true para fazer o calculo de IMC
+	if (alturaEhValida && pesoEhValido) {
+		//Calcula o IMC
+		var imc = peso / (altura * altura);
+
+		// Coloca o conteudo de texto da var imc na tdImc
+		tdImc.textContent = imc.toFixed(2);
+
+		//toFixed() limita o numero de casas decimais
+	}
+}
 
