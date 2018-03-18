d50ba764283248e97b4b05fda1de2dd4f85e82a7
Marcelo da Silva Sousa <marcelo.sousa1997@hotmail.com>
2018-03-18 15:10:21 -0300

    Aula 1: Boas Práticas

---
+++ introducao-javascript/js/principal.js
@@ -0,0 +1,9 @@
+//querySelector seleciona um elemento html,class,id
+var titulo = document.querySelector(".titulo");
+console.log(titulo);
+
+//textContent seleciona o conteúdo de texto da tag/variavel
+console.log(titulo.textContent);
+titulo.textContent = "Aparecida Nutricionista";
+
+console.log("Fui carregado de um arquivo externo");
