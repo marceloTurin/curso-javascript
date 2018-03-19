70f44faef40c3724fda2ee86506325b1c0c83f9e
Marcelo da Silva Sousa <marcelo.sousa1997@hotmail.com>
2018-03-18 22:44:58 -0300

    Aula 4: Adicionando pacientes na tabela

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -84,5 +84,38 @@ var botaoAdicionar = document.querySelector("#adicionar-paciente");
 botaoAdicionar.addEventListener("click",function(event){
 	//Não permite que o vento faça seu comportamento padrão, e reagira conforme o que pedimos no evento
 	event.preventDefault();
-	console.log("Oi eu sou um botão e fui clicado");
+	
+	var form = document.querySelector("#form-adiciona");
+
+	//Pega os valores do formulario da tag name do formulário
+	var nome = form.nome.value;
+	var peso = form.peso.value;
+	var altura = form.altura.value;
+	var gordura = form.gordura.value;
+
+	//Cria os elemto tr e td 
+	var pacienteTr = document.createElement("tr");
+
+	var nomeTd = document.createElement("td");
+	var pesoTd = document.createElement("td");
+	var alturaTd = document.createElement("td");
+	var gorduraTd = document.createElement("td");
+	var imcTd = document.createElement("td");
+
+	//Pega os elementos criados e coloca o conteudo de texto dos valores do formulario
+	nomeTd.textContent = nome;
+	pesoTd.textContent = peso;
+	alturaTd.textContent = altura;
+	gorduraTd.textContent = gordura;
+
+	//Insere os filhos td e coloca na tr
+	pacienteTr.appendChild(nomeTd);
+	pacienteTr.appendChild(pesoTd);
+	pacienteTr.appendChild(alturaTd);
+	pacienteTr.appendChild(gorduraTd);
+
+	// Insere o filho tr e coloca na tabela
+	var tabela = document.querySelector("#tabela-pacientes");
+	tabela.appendChild(pacienteTr);
+
 });
