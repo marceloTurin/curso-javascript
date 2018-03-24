d189afb56e70946d004111c6a873dac645d251ab
Marcelo da Silva Sousa <marcelo.sousa1997@hotmail.com>
2018-03-24 18:57:29 -0300

    Aula 5: Boas Práticas com Javascript

--- introducao-javascript/js/form.js
+++ introducao-javascript/js/form.js
@@ -22,45 +22,23 @@ botaoAdicionar.addEventListener("click",function(event){
 	var form = document.querySelector("#form-adiciona");
 
 	//Pega os valores do formulario da tag name do formulário
-
 	//Chama a função que obtem os dados do paciente e coloca em um objeto
 	var paciente = obtemPacienteDoFormulario(form);
-	console.log(paciente);
 
-
-	//Cria os elemto tr e td 
-	var pacienteTr = document.createElement("tr");
-
-	var nomeTd = document.createElement("td");
-	var pesoTd = document.createElement("td");
-	var alturaTd = document.createElement("td");
-	var gorduraTd = document.createElement("td");
-	var imcTd = document.createElement("td");
-
-
-	//Pega os elementos criados e coloca o conteudo de texto dos valores do formulario
-	nomeTd.textContent = nome;
-	pesoTd.textContent = peso;
-	alturaTd.textContent = altura;
-	gorduraTd.textContent = gordura;
-	imcTd.textContent = calculaImc(peso,altura);
-
-	//Insere os filhos td e coloca na tr
-	pacienteTr.appendChild(nomeTd);
-	pacienteTr.appendChild(pesoTd);
-	pacienteTr.appendChild(alturaTd);
-	pacienteTr.appendChild(gorduraTd);
-	pacienteTr.appendChild(imcTd);
-
+	//Função que criar a tr do paciente
+	var pacienteTr = montaTr(paciente);
 
 	// Insere o filho tr e coloca na tabela
 	var tabela = document.querySelector("#tabela-pacientes");
 	tabela.appendChild(pacienteTr);
 
+	//Reseta o formulario limpando os campos
+	form.reset();
+
 });
 
 
-//escuta o vento click quando o usuário clicar no titulo
+//escuta o evento click quando o usuário clicar no titulo
 
 //primeiro versão  usando uma função anônima
 titulo.addEventListener("click",function(){
@@ -76,7 +54,7 @@ function mostraMensagem(){
 
 
 
-
+//Função que pega a variavel form e cria em um objeto javascript do paciente
 function obtemPacienteDoFormulario(form){
 
 	var paciente = {
@@ -86,7 +64,41 @@ function obtemPacienteDoFormulario(form){
 		gordura: form.gordura.value,
 		imc :calculaImc(form.peso.value, form.altura.value)
 	}
-
 	return paciente;
+}
 
+//Função que pega o objeto paciente e monta as Tr da tabela
+function montaTr(paciente){
+
+	//Cria os elemto tr 
+	var pacienteTr = document.createElement("tr");
+	//Adiciona uma classe a tr
+	pacienteTr.classList.add("paciente");
+
+	//Variavies que cria a td chamando a função montaTd
+	var nomeTd = montaTd(paciente.nome, "info-nome");
+	var pesoTd = montaTd(paciente.peso, "info-peso");
+	var alturaTd = montaTd(paciente.altura, "info-altura");
+	var gorduraTd = montaTd(paciente.gordura, "info-gordura");
+	var imcTd = montaTd(paciente.imc, "info-imc");
+
+
+	//Insere os filhos td e coloca na tr
+	pacienteTr.appendChild(nomeTd);
+	pacienteTr.appendChild(pesoTd);
+	pacienteTr.appendChild(alturaTd);
+	pacienteTr.appendChild(gorduraTd);
+	pacienteTr.appendChild(imcTd);
+
+	return pacienteTr;
+
+}
+
+
+//Função que monta a Td com o dado que veio do formulário e com o nome da classe
+function montaTd(dado,classe){
+	var td = document.createElement("td");
+	td.textContent = dado;
+	td.classList.add(classe);
+	return td;
 }
