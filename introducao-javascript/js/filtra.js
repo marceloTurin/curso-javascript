77258975a9c01c7147de6f129fce941a1915e86e
Marcelo da Silva Sousa <marcelo.sousa1997@hotmail.com>
2018-03-30 17:24:46 -0300

    Implementando a lógica de filtragem

--- introducao-javascript/js/filtra.js
+++ introducao-javascript/js/filtra.js
@@ -6,9 +6,27 @@ campoFiltro.addEventListener("input",function(){
 	console.log(this.value);
 	var pacientes = document.querySelectorAll(".paciente");
 
-	for(var i = 0; i < pacientes.lenght ; i ++){
-		var paciente = pacientes[i];
-		var tdNome = paciente.querySelector(".info-nome");
-		var nome = paciente.textContent;
+
+	if (this.value.length > 0) { // Compara se o campo de texto é maior que zero
+		for(var i = 0; i < pacientes.length ; i ++){
+			var paciente = pacientes[i];
+			var tdNome = paciente.querySelector(".info-nome");
+			var nome = tdNome.textContent;
+
+			if (nome != this.value) { //Verifica se o texto  da td comparada é diferente da digitada
+				paciente.classList.add("invisivel");
+			}
+			else{
+				paciente.classList.remove("invisivel");
+			}
+		}
 	}
+	else{ // Se não tiver nada digitada no campo de filtro ele remove a classe invisivel
+		 for( var i = 0; i < pacientes.length; i++){
+		 	var paciente = pacientes[i];
+		 	paciente.classList.remove("invisivel");
+		 }
+	}
+
+	
 });
